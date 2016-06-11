1b21f186d1244fd9b66d457430e09a430b76c86e
manashee <manashee@gmail.com>
2016-06-11 17:40:07 +0400

    Add files via upload

---
+++ pytrip.jl
@@ -0,0 +1,14 @@
+println("-------------------")
+println("pythagorean tiplets")
+println("-------------------") 
+
+for i=0:10
+       o = 2i+1
+       s = 2i*(i+1)
+
+       
+       println(o , ", " , s , ", " , s+1 ) 
+       # println(o , " " , s , " " , s+1 , " " , o^2 ,   " " , o^2+s^2 , " " , (s+1)^2 ) 
+       
+       end
+
